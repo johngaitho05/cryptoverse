eea900c2a324dca17c0fcf680db3925e9a219455
John Gaitho <johngaitho05@gmail.com>
2023-03-03 23:41:37 +0300

    add footer and fix titles

--- src/components/CryptoCurrencies.jsx
+++ src/components/CryptoCurrencies.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import millify from "millify";
 import {Link} from 'react-router-dom'
 import {Card, Row, Col, Input} from 'antd'
@@ -9,9 +9,22 @@ const CryptoCurrencies = ({simplified}) => {
     const count = simplified ? 10 : 100
     const {data: cryptosList, isFetching} = useGetCryptosQuery(count)
     const [cryptos, setCryptos] = useState(cryptosList?.data?.coins)
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        const filteredData = cryptosList?.data?.coins.filter((coin) => {return coin.name.toLowerCase().includes(searchTerm.toLowerCase())})
+        setCryptos(filteredData)
+    }, [cryptosList,searchTerm]);
+
+
     if(isFetching) return 'Loading...'
+
     return (
         <>
+            {!simplified &&
+                <div className="search-crypto" >
+                    <Input placeholder="Search Cryptocurrency" onChange={(e) => {setSearchTerm(e.target.value)}}/>
+                </div>}
             <Row gutter={[32,32]} className="crypto-card-container">
                 {cryptos?.map((currency) => (
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -16,21 +16,21 @@ const Homepage = () => {
         <>
             <Title level={2} className="heading">Global Crypto Stats</Title>
             <Row>
-                <Col span="12"><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
-                <Col span="12"><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
-                <Col span="12"><Statistic title="Total market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
-                <Col span="12"><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
-                <Col span="12"><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
+                <Col key="cryptocurrencies" span="12"><Statistic title="Total Cryptocurrencies" value={globalStats.total}/></Col>
+                <Col key="exchanges" span="12"><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
+                <Col key="market-cap" span="12"><Statistic title="Total market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
+                <Col key="24h-volume" span="12"><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
+                <Col key="total-market" span="12"><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
             </Row>
             <div className="home-heading-container">
-                <Title levl={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
-                <Title levl={2} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
+                <Title level={2} className="home-title">Top 10 Cryptocurrencies in the world</Title>
+                <Title level={4} className="show-more"><Link to="/cryptocurrencies">Show More</Link></Title>
             </div>
             <CryptoCurrencies simplified/>
 
             <div className="home-heading-container">
-                <Title levl={2} className="home-title">Latest Crypto News</Title>
-                <Title levl={2} className="show-more"><Link to="/news">Show More</Link></Title>
+                <Title level={2} className="home-title">Latest Crypto News</Title>
+                <Title level={4} className="show-more"><Link to="/news">Show More</Link></Title>
             </div>
             <News simplified/>
         </>
