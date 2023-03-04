b7e108e2c9aa5423b90e6451da37339b75446686
John Gaitho <johngaitho05@gmail.com>
2023-03-04 12:42:15 +0300

    add crypto news reducer to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,16 @@
 import {configureStore} from "@reduxjs/toolkit";
 import {cryptoApi} from '../services/cryptoApi'
 import {setupListeners} from "@reduxjs/toolkit/query";
+import {cryptoNewsApi} from "../services/cryptoNewsApi";
 
 const store =  configureStore({
     reducer: {
-        [cryptoApi.reducerPath]: cryptoApi.reducer
+        [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer
     },
-    middleware: getDefaultMiddleware => getDefaultMiddleware().concat(cryptoApi.middleware)
+    middleware: getDefaultMiddleware => getDefaultMiddleware()
+        .concat(cryptoApi.middleware)
+        .concat(cryptoNewsApi.middleware)
 })
 
 setupListeners(store.dispatch)
