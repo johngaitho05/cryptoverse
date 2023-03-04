a519de84300842fea9a48cc986390fa8c0c65adc
John Gaitho <johngaitho05@gmail.com>
2023-03-04 12:45:05 +0300

    crypto details API

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -15,11 +15,15 @@ export const cryptoApi = createApi({
     endpoints: (builder) => ({
         getCryptos: builder.query({
             query: (count) => createRequest(`/coins?limit=${count}`)
-        })
+        }),
+        getCryptoDetails: builder.query({
+            query: (coindId) => createRequest(`/coin/${coindId}`)
+        }),
     })
 })
 
 export const {
     useGetCryptosQuery,
+    useGetCryptoDetailsQuery
 
 } = cryptoApi;
