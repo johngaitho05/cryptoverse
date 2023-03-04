8490e422c7a27f2b23301c26759f7e7e4a98cced
John Gaitho <johngaitho05@gmail.com>
2023-03-04 12:46:09 +0300

    fix crypto detais link

--- src/components/CryptoCurrencies.jsx
+++ src/components/CryptoCurrencies.jsx
@@ -28,7 +28,7 @@ const CryptoCurrencies = ({simplified}) => {
             <Row gutter={[32,32]} className="crypto-card-container">
                 {cryptos?.map((currency) => (
                     <Col xs={24} sm={12} lg={6} className="crypto-card" key={currency.id}>
-                        <Link to={`/crypto/${currency.id}`}>
+                        <Link to={`/crypto/${currency.uuid}`}>
                             <Card title={`${currency.rank}. ${currency.name}`}
                                   extra={<img src={currency.iconUrl} className="crypto-image"/>}
                                   hoverable
